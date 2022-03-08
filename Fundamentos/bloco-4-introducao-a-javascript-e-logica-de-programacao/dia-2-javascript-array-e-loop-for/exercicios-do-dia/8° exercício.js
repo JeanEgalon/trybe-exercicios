424c8a40946e2937dfe27b8def483f7222dba026
Jean Freitas <jeanjfe94@gmail.com>
2022-03-08 18:59:51 -0300

    Adicionado o oitavo exercicio

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/8° exercício.js
@@ -0,0 +1,9 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado
+
+let listinha = [];
+
+for (let contador = 1; contador < 26; contador += 1) {
+    listinha.push(contador);
+}
+
+console.log(listinha);
