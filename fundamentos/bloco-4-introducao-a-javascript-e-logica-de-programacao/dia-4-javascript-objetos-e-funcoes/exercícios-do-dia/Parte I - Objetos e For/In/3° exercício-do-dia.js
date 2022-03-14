ba1c5356b3dae987177bebd72932fdcc8967df5c
Jean Freitas <jeanjfe94@gmail.com>
2022-03-14 15:43:29 -0300

    Criado as pastas para o conteudo do dia

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/Parte I - Objetos e For/In/3° exercício-do-dia.js
@@ -0,0 +1,22 @@
+// Parte I - Objetos e For/In
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info["recorrente"] = "Sim";
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+// personagem
+// origem
+// nota
+// recorrente
+
+for (let showMe in info) {
+    console.log(showMe);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/Parte I - Objetos e For/In/8° exercício-do-dia.js
@@ -0,0 +1,21 @@
+// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+        {
+            titulo: 'O Pior Dia de Todos',
+            autor: 'Daniela Kopsch',
+            editora: 'Tordesilhas',
+        },
+        {
+            titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+            autor: 'JK Rowling',
+            editora: 'Rocco',
+        }
+    ],
+};
+
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos");
