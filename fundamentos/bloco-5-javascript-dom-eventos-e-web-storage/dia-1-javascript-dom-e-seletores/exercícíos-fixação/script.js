f6408f3f37f3118e2f0e77858744119fc0658986
Jean Freitas <jeanjfe94@gmail.com>
2022-03-14 19:42:55 -0300

    Adicionado o terceiro e ultimo exercicio de fixaçao

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercícíos-fixação/script.js
@@ -0,0 +1,10 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+document.querySelector('.emergency-tasks h3').style.backgroundColor = 'purple';
+document.querySelectorAll('.emergency-tasks h3')[1].style.backgroundColor = 'purple';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'orange';
+
+document.querySelector('.no-emergency-tasks h3').style.backgroundColor = 'black';
+document.querySelectorAll('.no-emergency-tasks h3')[1].style.backgroundColor = 'black';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
