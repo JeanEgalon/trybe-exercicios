80a86433e0598bf06e4ebe15dc1be6347265c744
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 23:01:13 -0300

    Update 4.2 - Operadores lógicos - OR.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/4.2 - Operadores lógicos - OR.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/4.2 - Operadores lógicos - OR.js
@@ -1,11 +1,4 @@
-// Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
-
 let weekDay = "sexta-feira";
-
-// Implemente condicionais para que:
-// Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima 
-// "Oba, mais um dia de aprendizado na Trybe >:D".
-// Se for algum dia de fim de semana, imprima "FINALMENTE, descanso merecido UwU".
 
 if (weekDay === "segunda-feira" || weekDay === "terça-feira" || weekDay === "quarta-feira" || weekDay === "quinta-feira" || weekDay === "sexta-feira") {
     console.log("Oba, mais um dia de aprendizado na Trybe >:D")
