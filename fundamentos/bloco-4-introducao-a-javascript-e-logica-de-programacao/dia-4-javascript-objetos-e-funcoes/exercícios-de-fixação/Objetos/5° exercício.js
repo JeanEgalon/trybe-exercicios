56266a9192a4990f04a7fe8d76c7930c98df3484
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:54:20 -0300

    Update 5° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Objetos/5° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Objetos/5° exercício.js
@@ -1,8 +1,3 @@
-// 5 - Acesse a chave medals e faça um console.log no seguinte formato: "A jogadora possui 2 medalhas de ouro e 3 medalhas de prata".
-// Agora veremos uma variação do laço for , que nos garante facilidade ao lidar com objetos.
-
-// 4 - Acesse a chave bestInTheWorld e faça um console.log no seguinte formato: "A jogadora Marta Silva foi eleita a melhor do mundo por 6 vezes".
-
 let player = {
     name : 'Marta',
     lastName : 'Silva',
