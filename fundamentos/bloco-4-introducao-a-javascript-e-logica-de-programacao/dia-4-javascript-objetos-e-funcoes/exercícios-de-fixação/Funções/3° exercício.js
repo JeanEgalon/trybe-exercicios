caa5d9e6653d6c95e6f7dfdc7bf3a26669a790c0
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:53:13 -0300

    Update 3° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/3° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/3° exercício.js
@@ -1,5 +1,3 @@
-// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
-
 const a = 7;
 const b = 5;
 const c = 7;
