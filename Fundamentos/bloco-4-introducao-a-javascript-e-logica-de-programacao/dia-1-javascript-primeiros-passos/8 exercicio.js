9f73d15037ea9c83df3d3778ac3a05aaef72f643
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 23:00:18 -0300

    Update 8 exercicio.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/8 exercicio.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/8 exercicio.js
@@ -1,6 +1,3 @@
-// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
-// Bonus: use somente um if .
-
 const a = 2;
 const b = 3;
 const c = 4;
