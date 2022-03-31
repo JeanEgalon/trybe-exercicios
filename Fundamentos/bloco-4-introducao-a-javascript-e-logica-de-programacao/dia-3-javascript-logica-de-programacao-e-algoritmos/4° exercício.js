323620dae5dac820d2ad19f125d826e771ffddbd
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:51:49 -0300

    Update 4° exercício.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4° exercício.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/4° exercício.js
@@ -1,6 +1,3 @@
-// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50
-
-
 let maiorNumeroPrimo = 0;
 
 for (let primeiroNumeroQueSeraComparado = 0; primeiroNumeroQueSeraComparado <= 50; primeiroNumeroQueSeraComparado += 1) {
