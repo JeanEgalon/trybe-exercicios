a96e87d9b4d0d8ad4f5a4edcd1f11acd5732f930
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:55:47 -0300

    Update 4° exercício-do-dia.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/4° exercício-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/4° exercício-do-dia.js
@@ -1,7 +1,3 @@
-// Parte I - Objetos e For/In
-
-// Usando o objeto abaixo, faça os exercícios a seguir:
-
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -9,13 +5,6 @@ let info = {
 };
 
 info["recorrente"] = "Sim";
-
-// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
-
-// Margarida
-// Pato Donald
-// Namorada do personagem principal nos quadrinhos do Pato Donald
-// Sim
 
 for (let showMe in info) {
     console.log(info[showMe]);
