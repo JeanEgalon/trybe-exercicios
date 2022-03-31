5035d9d3b1f4804369135db449150d667b7bc075
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:59:35 -0300

    Update 3 exercicio.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/3 exercicio.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/3 exercicio.js
@@ -1,5 +1,3 @@
-// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
-
 const a = 5;
 const b = 10;
 const c = 10;
