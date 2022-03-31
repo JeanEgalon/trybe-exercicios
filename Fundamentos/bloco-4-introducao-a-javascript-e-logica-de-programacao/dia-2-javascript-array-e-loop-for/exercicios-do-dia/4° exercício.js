8ab71ff73f5d2746191f24976335f986d8d17c37
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:49:06 -0300

    Update 4° exercício.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/4° exercício.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/4° exercício.js
@@ -1,9 +1,4 @@
-// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, 
-// imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
 
 let qntNumeros = numbers.length;
 let soma = 0;
