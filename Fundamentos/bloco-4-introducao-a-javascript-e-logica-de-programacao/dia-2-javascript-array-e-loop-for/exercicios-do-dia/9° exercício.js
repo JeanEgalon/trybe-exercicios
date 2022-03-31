5e3607bbc7019847e592ab1232c29a42acd6a327
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:50:40 -0300

    Update 9° exercício.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/9° exercício.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/9° exercício.js
@@ -4,8 +4,6 @@ for (let contador = 1; contador < 26; contador += 1) {
     listinha.push(contador);
 }
 
-// console.log(listinha);
-
 for (let contador2 = 0; contador2 < listinha.length; contador2 += 1) {
     console.log (listinha[contador2] / 2);
 }
