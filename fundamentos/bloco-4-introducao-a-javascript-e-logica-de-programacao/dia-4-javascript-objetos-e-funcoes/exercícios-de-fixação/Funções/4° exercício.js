99f6af01e07d35338b44130e94281a55122de8b2
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:53:23 -0300

    Update 4° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/4° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/4° exercício.js
@@ -1,5 +1,3 @@
-// Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-
 const a = 0;
 
 function valorDefinido(a) {
