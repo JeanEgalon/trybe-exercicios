896d0914cf9ae36544621f8d0a450a610278d5f8
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:56:10 -0300

    Update 6° exercício-do-dia.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/6° exercício-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/6° exercício-do-dia.js
@@ -1,5 +1,3 @@
-// Usando o objeto abaixo, faça os exercícios a seguir:
-
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
@@ -12,8 +10,5 @@ let leitor = {
         },
     ],
 };
-
-// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: 
-// "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
 console.log("O livro favorito de " + leitor['nome'] + " " + leitor['sobrenome'] + " se chama " + leitor['livrosFavoritos'][0]['titulo']);
