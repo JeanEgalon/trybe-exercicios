b0363b9252391e548f2ab27a19ed6ca2cdba3a2a
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 23:05:35 -0300

    Update script.js

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercício-do-dia/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercício-do-dia/script.js
@@ -10,15 +10,12 @@ var picker = new Pikaday({
     field: document.getElementById('datepicker'),
     format: 'D/M/YYYY',
     toString(date, format) {
-        // you should do formatting based on the passed format,
-        // but we will just return 'D/M/YYYY' for simplicity
         const day = date.getDate();
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
         return `${day}/${month}/${year}`;
     },
     parse(dateString, format) {
-        // dateString is the result of `toString` method
         const parts = dateString.split('/');
         const day = parseInt(parts[0], 10);
         const month = parseInt(parts[1], 10) - 1;
