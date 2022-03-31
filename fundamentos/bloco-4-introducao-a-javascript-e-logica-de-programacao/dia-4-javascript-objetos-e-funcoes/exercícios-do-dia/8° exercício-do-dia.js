a23600f1ba07fc75ec867ee637e207927fb222b7
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:56:29 -0300

    Update 8° exercício-do-dia.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/8° exercício-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-do-dia/8° exercício-do-dia.js
@@ -1,5 +1,3 @@
-// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
-
 let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
