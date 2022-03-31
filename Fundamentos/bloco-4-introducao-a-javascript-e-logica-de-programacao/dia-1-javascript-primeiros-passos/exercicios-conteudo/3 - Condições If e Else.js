860e5e7fa49befc2c6fb62bb1092885f3653096f
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 23:00:47 -0300

    Update 3 - Condições If e Else.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/3 - Condições If e Else.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/3 - Condições If e Else.js
@@ -1,18 +1,4 @@
-// Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
-
 const nota = 80;
-
-// Implemente uma lógica que verifique se a pessoa candidata foi aprovada, 
-// reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
-
-// Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
-// Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
-// Se a nota for menor que 60, imprima "Você foi reprovada(o)"
-
-// Crie uma estrutura condicional utilizando o if , else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação.
-
-// Altere o valor da nota para verificar se as condições que você implementou funcionam;
-
 
 if (nota >= 80) {
     console.log("Parabéns, você foi aprovada(o)!");
