c55293b2fa72c0588e637750b4a9c2acfd5ebed5
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:52:50 -0300

    Update 1° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/1° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Funções/1° exercício.js
@@ -1,12 +1,3 @@
-// Faça cinco programas, um para cada operação aritmética básica. 
-// Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
-
-// Adição (a + b)
-// Subtração (a - b)
-// Multiplicação (a * b)
-// Divisão (a / b)
-// Módulo (a % b)
-
 let a = 2;
 let b = 2;
 
