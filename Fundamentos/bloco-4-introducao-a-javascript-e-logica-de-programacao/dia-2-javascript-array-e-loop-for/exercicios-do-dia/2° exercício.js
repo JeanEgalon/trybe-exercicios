ff1bc4c0a5f9061e678f8b1e8a7df9c198cf04a2
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:48:46 -0300

    Update 2° exercício.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/2° exercício.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios-do-dia/2° exercício.js
@@ -1,8 +1,4 @@
-// Leia atentamente os enunciados e faça o que se pede! Iremos utilizar esse array para realizar os exercícios do 1 ao 7:
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
 let soma = 0;
 
