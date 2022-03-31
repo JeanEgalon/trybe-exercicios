d55f79a8f59bdec09f3e04ad659374f9dd264aaa
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:54:32 -0300

    Update 1° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/for/in e for/of/1° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/for/in e for/of/1° exercício.js
@@ -1,5 +1,3 @@
-// 1 - Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
-
 let names = {
     person1: 'João',
     person2: 'Maria',
