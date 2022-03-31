b2e7b44474cc9728a9fa32efa0d0c5585e86840a
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:53:56 -0300

    Update 2° exercício.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Objetos/2° exercício.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercícios-de-fixação/Objetos/2° exercício.js
@@ -1,6 +1,3 @@
-// 2 - Acesse as chaves name , lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: 
-// "A jogadora Marta Silva tem 34 anos de idade".
-
 let player = {
     name : 'Marta',
     lastName : 'Silva',
