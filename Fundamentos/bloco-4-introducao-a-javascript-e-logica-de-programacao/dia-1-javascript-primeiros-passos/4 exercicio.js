94c28237b9b131385b4c5b269c51d265877fa9c5
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 22:59:42 -0300

    Update 4 exercicio.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4 exercicio.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/4 exercicio.js
@@ -1,5 +1,3 @@
-// Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-
 const a = -10;
 
 if (a > 0) {
