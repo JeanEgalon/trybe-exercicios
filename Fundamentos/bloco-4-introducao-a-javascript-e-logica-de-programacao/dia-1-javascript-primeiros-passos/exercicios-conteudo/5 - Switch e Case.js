db38c1b3466f6faab7162354a1c891908f37d112
Jean de Freitas Egalon <jeanjfe94@gmail.com>
2022-03-30 23:01:23 -0300

    Update 5 - Switch e Case.js

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/5 - Switch e Case.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios-conteudo/5 - Switch e Case.js
@@ -1,9 +1,4 @@
-// Crie uma variável para armazenar o estado da pessoa candidata no processo seletivo, que pode ser: 'aprovada' , 'lista' ou 'reprovada' ;
-
 let candidata = "aprovada";
-
-// Crie uma estrutura condicional com o switch/case que irá imprimir as mensagens do exercício anterior
-//  se o estado da pessoa candidata for 'aprovada' , 'lista' ou 'reprovada' . Como default , imprima a mensagem de 'não se aplica' .
 
 switch (candidata) {
     case "aprovada":
