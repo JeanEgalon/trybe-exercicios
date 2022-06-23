7b4d5470cd4ee5f6562c456eff674075e5dac98e
Jean Freitas <jeanjfe94@gmail.com>
2022-06-23 05:06:24 -0300

    feito os exercicios do dia

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-1-e-2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Task = (value) => {
+  return <li>{value}</li>;
+};
+
+const compromissos = ['estudar', 'projeto', 'estudar mais'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>
+        { compromissos.map(compro => Task(compro)) }
+      </ul>
+      )
+  }
+}
+
+export default App;
