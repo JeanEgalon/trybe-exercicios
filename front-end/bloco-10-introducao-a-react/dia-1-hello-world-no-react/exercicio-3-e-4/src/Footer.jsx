f2524ba0cfea84e25c3443f39c77081408cbf508
Jean Freitas <jeanjfe94@gmail.com>
2022-06-23 05:05:47 -0300

    feito os exercicios do dia

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3-e-4/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>E isso é só o começo...</h1>
+      </>
+    );
+  }
+}
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-3-e-4/src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Conteúdos de Front-End</h1>
+      </>
+    );
+  }
+}
