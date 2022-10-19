f294bccab1fbe6a25324e73b2c627dfb99d90210
Jean Egalon <egalon.jean@gmail.com>
2022-10-19 18:48:27 -0300

    feito o segundo exercício

--- 02 - Back End/secao-06--Node.js:-ORM-e-Autenticacao/Dia-01:-ORM-Interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/migrations/20221019213252-create-books.js
+++ 02 - Back End/secao-06--Node.js:-ORM-e-Autenticacao/Dia-01:-ORM-Interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/migrations/20221019213252-create-books.js
@@ -1,8 +1,7 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Books', {
       id: {
         allowNull: false,
@@ -34,6 +33,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('books')
+    await queryInterface.dropTable('Books')
   }
 };
