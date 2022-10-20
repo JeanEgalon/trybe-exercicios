a3258e158feb551da44ed6a37feb805fab7a7baa
Jean Freitas <egalon.jean@gmail.com>
2022-10-20 19:54:32 -0300

    Delete 02 - Back End/secao-06--Node.js:-ORM-e-Autenticacao/Dia-01:-ORM-Interface-da-aplicacao-com-o-banco-de-dados/exercicios directory

--- 02 - Back End/secao-06--Node.js:-ORM-e-Autenticacao/Dia-01:-ORM-Interface-da-aplicacao-com-o-banco-de-dados/exercicios/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-const { bookRouter } = require('./src/routers')
-
-app.use(express.json());
-app.use('/books', bookRouter);
-
-app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
--- 02 - Back End/secao-06--Node.js:-ORM-e-Autenticacao/Dia-01:-ORM-Interface-da-aplicacao-com-o-banco-de-dados/exercicios/src/routers/index.js
+++
@@ -1,3 +0,0 @@
-const bookRouter = require('./books.router');
-
-module.exports = { bookRouter };
